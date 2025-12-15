ca781f6d1841fcbbcc5686c60b0916623cb81cb1
SiyaM108 <deprominent@gmail.com>
2025-12-15 14:47:30 +0000

    complete call-me-later.js

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,5 +1,19 @@
 // Your code here
+class CallCenter {
+	constructor(name) {
+		this.name = name;
+	}
 
+	sayHello() {
+		return `Hello, this is ${this.name}`;
+	}
+
+	callMeLater(delay) {
+		setTimeout(() => {
+			console.log(this.sayHello());
+		}, delay);
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
