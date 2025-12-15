001b848be4e1415d3fde2a924f9ca5f7673ddd98
SiyaM108 <deprominent@gmail.com>
2025-12-15 14:14:26 +0000

    complete calculator.js

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,41 @@
 // Your code here
+class Calculator {
+	constructor() {
+		this.total = 0;
+	}
+
+	add(number) {
+		this.total += number;
+		return this.total;
+	}
+
+	subtract(number) {
+		this.total -= number;
+		return this.total;
+	}
+
+	multiply(number) {
+		this.total *= number;
+		return this.total;
+	}
+
+	divide(number) {
+		if (number === 0) {
+			throw new Error("Cannot divide by zero");
+		}
+		this.total /= number;
+		return this.total;
+	}
+
+	reset() {
+		this.total = 0;
+		return this.total;
+	}
+
+	getTotal() {
+		return this.total;
+	}
+}
 
 
 /*****************************************************************************/
